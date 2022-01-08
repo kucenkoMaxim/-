543bc3b0da77e143b76c33796dbbf31ff0603da8
kucenkoMaxim <96391525+kucenkoMaxim@users.noreply.github.com>
2022-01-08 18:09:13 +0200

    first commit

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    companion object{
+        const val NAME="NAME"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+    val textView=findViewById<TextView>(R.id.text_view)
+        val name = intent.getStringExtra(NAME)
+
+        textView.text=name
+
+
+    }
+    fun back(view:View) {
+        onBackPressed()
+    }
+
+
+}
